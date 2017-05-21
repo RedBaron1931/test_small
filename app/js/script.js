349e3158500ad8e626c627366b1fea225726f477
Sergey Dudnichenko <redbaron1931@gmail.com>
2017-05-21 21:09:22 +0300

    Made some changes

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,13 @@
+$(function(){	
 
-$('.owl_carousel').owlCarousel({
-	singleItem: true,
-	navigation: true,
-	navigationText: ['<img src="img/arrow_left.png" alt="">','<img src="img/arrow_right.png" alt="">']
-
+	$(".sale_box .wrapper .tab").click(function() {
+		$(".sale_box .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+ 		$(".sale_box .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+	
+	$('.owl_carousel').owlCarousel({
+ 	singleItem: true,
+  	navigation: true,
+ 	navigationText: ['<img src="img/left.png" alt="">','<img src="img/right.png" alt="">']
+	});
 });
-  $(document).ready(function(){ 
-        var my_link = location.pathname;
-        $('#top_menu li a[href="'+my_link+'"]').parent().addClass('active');
-    });
-
